2d42a54a17f575aad7c3589252f919892de821bc
구자건 <147472859+AruJoy@users.noreply.github.com>
2024-03-25 12:00:16 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 136 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -0,0 +1,29 @@
+from sys import stdin
+num_inputs = stdin.readline()
+num_inputs = int(num_inputs)
+inputList = list()
+
+for i in range(num_inputs):
+    inputList.append(int(stdin.readline()))
+
+primeList = 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+def isPrime(number, primeList):
+    if number == 1:
+        return False
+    if number in primeList:
+        return True
+    for prime in primeList:
+        if number % prime == 0:
+            return False
+    return True
+
+def printAnswer(primeList, number):
+    checkRange = number // 2
+    for i in range(checkRange):
+        value = checkRange - i
+        if isPrime(value, primeList) and isPrime(number - value, primeList):
+            print(f'{value} {number - value}')
+            return
+
+for i in inputList:
+    printAnswer(primeList, i)
