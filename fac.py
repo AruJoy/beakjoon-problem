e5360a694adf048671ceb8f61044c80be0cd7b50
구자건 <wkrjsdl5632@naver.com>
2024-03-26 17:06:25 +0900

    머지소트 구현 성공

--- fac.py
+++ fac.py
@@ -1,14 +1,35 @@
 from sys import stdin
-n = int(stdin.readline())
-def hanoi(n, start, sub, target):
-    if n == 1:
-        print(f'{start} {target}')
-        return
-    hanoi(n-1, start, target, sub)
-    print(f'{start} {target}')
-    hanoi(n-1, sub, start, target)
-sqr = 2**n
-print(f'{sqr - 1}')
-if n < 21 :
-    hanoi(n, 1, 2, 3)
-    #개같은거!
+def mergeSort(a):
+    if len(a) <= 1:
+        return a
+    middlePoint = len(a)//2
+    pl = 0
+    pr = 0
+    newList = list()
+    leftList = mergeSort(a[:middlePoint])
+    rightList = mergeSort(a[middlePoint:])
+    while len(newList) < len(leftList) + len(rightList):
+        if pr >= len(rightList):
+            newList.append(leftList[pl])
+            pl += 1
+        elif pl >= len(leftList):
+            newList.append(rightList[pr])
+            pr += 1
+        else:
+            if leftList[pl] < rightList[pr]:
+                newList.append(leftList[pl])
+                pl += 1
+            else: 
+                newList.append(rightList[pr])
+                pr += 1
+    return newList
+
+inputSize = int(stdin.readline())
+inputList = list()
+
+for i in range(inputSize):
+    inputList.append(int(stdin.readline()))
+
+saltedCaramel = mergeSort(inputList)
+for i in saltedCaramel:
+    print(i)
