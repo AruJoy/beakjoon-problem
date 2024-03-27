8b3a8d2441cd2681362176c80f7a1ebbc2a7f21d
구자건 <wkrjsdl5632@naver.com>
2024-03-27 10:43:54 +0900

    정답: 성공 but, 시간초과ㅜ

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,34 +1,14 @@
 from sys import stdin
-def quick(a, left, right):
-    pivot = a[(left + right) // 2]
-    pl = left
-    pr = right
-    while pl<=pr:
-        while a[pl] < pivot:
-            pl += 1
-        while pivot < a[pr]:
-            pr -= 1
-        if pl<=pr:
-            a[pl], a[pr] = a[pr], a[pl]
-            pl += 1
-            pr -= 1
+
+def sort(a):
+    a = a.sort()
+    for i in range(len(a)):
+        for j in range(i+1, len(a)):
+            if a[i] == a[j]:
+                a.remove(j)
     
-    if left < pr:
-        quick(a, left, pr)
-    if pl < right:
-        quick(a, pl, right)
-    return
-
-def qsort(a):
-    quick(a, 0, len(a)-1)
-
-
 numberOfSize = int(stdin.readline())
 inputList = list()
 for i in range(numberOfSize):
-    inputList.append(int(stdin.readline()))
-
-qsort(inputList)
-
-for i in inputList:
-    print(i)
+    inputList.append(stdin.readline())
+sort(inputList)
