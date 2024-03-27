770eff33a0b69024a2e729b321aeb0f221baae5a
구자건 <147472859+AruJoy@users.noreply.github.com>
2024-03-27 16:07:24 +0900

    [Gold IV] Title: 사냥꾼, Time: 688 ms, Memory: 54872 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.py
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.py
@@ -1,51 +1,28 @@
-from sys import stdin
-inputInformation = stdin.readline()
-inputInformation = inputInformation.split(' ')
+import sys
 
-rangeLength = int(inputInformation[2])
-numberOfAnimals = int(inputInformation[1])
-animals = list()
-
-gunPorts = stdin.readline()
-gunPorts = gunPorts.split(' ')
-gunPorts.sort()
-
-for i in range(len(gunPorts)):
-    gunPorts[i] = int(gunPorts[i])
-
-for i in range(numberOfAnimals):
-    inputValue = stdin.readline()
-    inputValue = inputValue.split(' ')
-    animals.append([int(inputValue[0]), int(inputValue[1])])
-
-
-def isKilledInRange(animal,length):
-    x1 = animal[0] - length + animal[1]
-    x2 = animal[0] + length - animal[1]
-    return[x1, x2]
-
-def isKilled(dangerZone, gunPorts):
-    left = 0
-    right = len(gunPorts)-1
-    while left <=right:
-        mid = (left + right) // 2
-
-        if dangerZone[0] <= gunPorts[mid] <= dangerZone[1]:
-            return True
-        elif gunPorts[mid] < dangerZone[0]:
-            left = mid + 1
-        elif dangerZone[1] < gunPorts[mid]:
-            right = mid - 1
-    return False
-
-def numberOfKilledAnimals(animals, gunPorts, length):
-    count = 0
-    for animal in animals:
-        if animal[1] > length:
-            continue
-        dangerZone = isKilledInRange(animal, length)
-        if isKilled(dangerZone, gunPorts):
+# 첫줄 입력 M은 사대의 수 N은 동물의 수 L은 사정거리
+M, N, L = map(int, sys.stdin.readline().split())
+# 두 번쨰 줄 입력은 사대의 좌표 입력
+saro = list(map(int, sys.stdin.readline().split()))
+# 그 이후 줄들은 동물의 좌표
+animal = [list(map(int, sys.stdin.readline().split())) for i in range(N)]
+saro.sort()
+count = 0
+for a, b in animal:
+    if b > L:  # 동물 좌표 b가 L보다 크면 잡을수없다.
+        continue
+    start = 0
+    end = len(saro) - 1
+    # 동물을 잡을수있는 최소값, 최대값
+    Min = a + b - L
+    Max = a - b + L
+    while start <= end:
+        mid = (start + end) // 2
+        if Min <= saro[mid] <= Max: # 동물을 잡을수있을떄 count+=1
             count += 1
-    return count
-
-print(numberOfKilledAnimals(animals, gunPorts, rangeLength))
+            break
+        elif saro[mid] < Max: # 동물을 못잡고 Max보다 작을떄
+            start = mid + 1
+        else:
+            end = mid - 1
+print(count)
