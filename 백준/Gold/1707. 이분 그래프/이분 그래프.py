6fa4d72bf4846e60f7923095af07b009033f1787
구자건 <147472859+AruJoy@users.noreply.github.com>
2024-04-01 22:23:22 +0900

    [Gold IV] Title: 이분 그래프, Time: 1144 ms, Memory: 51132 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,35 +1,43 @@
 from sys import stdin
 from collections import deque
 
-def is_bipartite(V, E, edges):
-    graph = [[] for _ in range(V + 1)]
-    for edge in edges:
-        node1, node2 = edge
-        graph[node1].append(node2)
-        graph[node2].append(node1)
-
-    colors = [0] * (V + 1)
-    for i in range(1, V + 1):
-        if colors[i] != 0:
-            continue
-
-        colors[i] = 1
-        queue = deque([i])
-        while queue:
-            node = queue.popleft()
-            for neighbor in graph[node]:
-                if colors[neighbor] == 0:
-                    colors[neighbor] = -colors[node]
-                    queue.append(neighbor)
-                elif colors[neighbor] == colors[node]:
+def bipartiteGraph():
+    V,E = map(int, stdin.readline().split(' '))
+    vertex = [list() for i in range(V+1)]
+    node_label = [None for i in range(V+1)]
+    node_label[0] = 0
+    for i in range(E):
+        node1, node2 = map(int, stdin.readline().split(' '))
+        vertex[node2].append(node1)
+        vertex[node1].append(node2)
+    for start in range(1, V+1):
+        if node_label[start] == None:
+            node_label[start] = 1
+            stack = deque()
+            current_node = start
+            current_node_label = node_label[start]
+            
+        for edge in vertex[start]:
+            if node_label[edge] == None:
+                node_label[edge] = -current_node_label
+                stack.append(edge)
+        while stack:
+            current_node = stack.pop()
+            current_node_label = node_label[current_node]
+            for edge in vertex[current_node]:
+                if node_label[edge] == None:
+                    node_label[edge] = - current_node_label
+                    stack.append(edge)
+                elif current_node_label == node_label[edge]:
                     return False
     return True
 
-number_of_tests = int(stdin.readline())
-for _ in range(number_of_tests):
-    V, E = map(int, stdin.readline().split())
-    edges = [tuple(map(int, stdin.readline().split())) for _ in range(E)]
-    if is_bipartite(V, E, edges):
+number_of_test = int(stdin.readline())
+result_set = list()
+for t in range(number_of_test):
+    result_set.append(bipartiteGraph())
+for i in result_set:
+    if i == True:
         print('YES')
     else:
         print('NO')
