8f7c0b4c446db3e73a944bfee2bf7fba2fee14ac
구자건 <wkrjsdl5632@gmail.com>
2024-04-03 15:57:37 +0900

    미로찾기 완성

--- prac.py
+++ prac.py
@@ -1,61 +1,47 @@
 from sys import stdin
 from collections import deque
-def find_lighter_than_middle(lighter_list, middle_condition):
-    expired_balls = 0
-    for ball in lighter_list:
-        lighter_balls = 0
-        stack = deque()
-        checked_ball = [False for i in range(balls+1)]
-        for i in ball:
-            if not checked_ball[i]:
-                stack.append(i)
-                lighter_balls += 1
-                checked_ball[i] = True
-        while stack:
-            inner_ball = stack.pop()
-            for i in lighter_list[inner_ball]:
-                if not checked_ball[i]:
-                    stack.append(i)
-                    lighter_balls += 1
-                    checked_ball[i] = True
-        if lighter_balls >= middle_condition:
-            expired_balls += 1
-    return expired_balls
+from heapq import heappop, heappush
+columns, rows = map(int, stdin.readline().split(' '))
+maze = list()
 
-def find_heavier_than_middle(heavier_list, middle_condition):
-    expired_balls = 0
-    for ball in heavier_list:
-        heavier_balls = 0
-        stack = deque()
-        checked_ball = [False for i in range(balls+1)]
-        for i in ball:
-            if not checked_ball[i]:
-                stack.append(i)
-                heavier_balls += 1
-                checked_ball[i] = True
-        while stack:
-            inner_ball = stack.pop()
-            for i in heavier_list[inner_ball]:
-                if not checked_ball[i]:
-                    stack.append(i)
-                    heavier_balls += 1
-                    checked_ball[i] = True
-        if heavier_balls >= middle_condition:
-            expired_balls += 1
-    return expired_balls
+for i in range(columns):
+    maze.append(list(map(int, stdin.readline().rstrip())))
+def find_path(maze):
+    start_node = [0,0]
+    que = deque([start_node])
+    
+    while que:
+        current_coordinate = que.popleft()
+        y = current_coordinate[0]
+        x = current_coordinate[1]
+        if 0 < y:
+            if maze[y-1][x] == 1:
+                que.append([y-1,x])
+                maze[y-1][x] = maze[y][x]+1
+            elif maze[y][x]+1 < maze[y-1][x]:
+                maze[y-1][x] = maze[y][x]+1
+                que.append([y-1,x])
+        if y < columns-1:
+            if maze[y+1][x] == 1:
+                que.append([y+1,x])
+                maze[y+1][x] = maze[y][x]+1
+            elif maze[y][x]+1 < maze[y+1][x]:
+                maze[y+1][x] = maze[y][x]+1
+                que.append([y+1,x])
+        if 0 < x:
+            if maze[y][x-1] == 1:
+                que.append([y,x-1])
+                maze[y][x-1] = maze[y][x]+1
+            elif maze[y][x]+1 < maze[y][x-1]:
+                maze[y][x-1] = maze[y][x]+1
+                que.append([y,x-1])
+        if x < rows-1:
+            if maze[y][x+1] == 1:
+                que.append([y,x+1])
+                maze[y][x+1] = maze[y][x]+1
+            elif maze[y][x]+1 < maze[y][x+1]:
+                maze[y][x+1] = maze[y][x]+1
+                que.append([y,x+1])
 
-
-balls, edges = map(int, stdin.readline().split(' '))
-heavier_list = [list() for i in range(balls + 1)]
-lighter_list = [list() for i in range(balls + 1)]
-middle_condition = (balls+1) // 2 
-for i in range(edges):
-    heavier, lighter = map(int, stdin.readline().split(' '))
-    heavier_list[lighter].append(heavier)
-    lighter_list[heavier].append(lighter)
-
-result = 0
-result += find_lighter_than_middle(lighter_list, middle_condition)
-result += find_heavier_than_middle(heavier_list, middle_condition)
-
-print(result)
+find_path(maze)
+print(maze[columns-1][rows-1])
