6676810dfa9d9c5ad5ce7caab8ab8a0fe0f1e078
구자건 <wkrjsdl5632@gmail.com>
2024-04-03 18:48:19 +0900

    특정 거리의 도시 찾기

--- prac.py
+++ prac.py
@@ -1,31 +1,47 @@
 from sys import stdin
 from collections import deque
-from heapq import heappop, heappush, heapify
+from heapq import heappop, heappush
+columns, rows = map(int, stdin.readline().split(' '))
+maze = list()
 
-def dijkstra(adj, start):
-    distance[start] = 0
-    min_distance = list()
-    min_distance.append([0, start])
+for i in range(columns):
+    maze.append(list(map(int, stdin.readline().rstrip())))
+def find_path(maze):
+    start_node = [0,0]
+    que = deque([start_node])
+    
+    while que:
+        current_coordinate = que.popleft()
+        y = current_coordinate[0]
+        x = current_coordinate[1]
+        if 0 < y:
+            if maze[y-1][x] == 1:
+                que.append([y-1,x])
+                maze[y-1][x] = maze[y][x]+1
+            elif maze[y][x]+1 < maze[y-1][x]:
+                maze[y-1][x] = maze[y][x]+1
+                que.append([y-1,x])
+        if y < columns-1:
+            if maze[y+1][x] == 1:
+                que.append([y+1,x])
+                maze[y+1][x] = maze[y][x]+1
+            elif maze[y][x]+1 < maze[y+1][x]:
+                maze[y+1][x] = maze[y][x]+1
+                que.append([y+1,x])
+        if 0 < x:
+            if maze[y][x-1] == 1:
+                que.append([y,x-1])
+                maze[y][x-1] = maze[y][x]+1
+            elif maze[y][x]+1 < maze[y][x-1]:
+                maze[y][x-1] = maze[y][x]+1
+                que.append([y,x-1])
+        if x < rows-1:
+            if maze[y][x+1] == 1:
+                que.append([y,x+1])
+                maze[y][x+1] = maze[y][x]+1
+            elif maze[y][x]+1 < maze[y][x+1]:
+                maze[y][x+1] = maze[y][x]+1
+                que.append([y,x+1])
 
-    while min_distance:
-        current_distance, current_city = heappop(min_distance)
-        if current_distance > distance[current_city]:
-            continue
-        for next_city in adj[current_city]:
-            if distance[next_city] > current_distance + 1:
-                distance[next_city] = current_distance + 1
-                heappush(min_distance, (distance[next_city], next_city))
-
-cities, edges, target_distance, start = map(int, stdin.readline().split(' '))
-distance = [10**9 for _ in range(cities+1)]
-adj = [list() for i in range(cities+1)]
-for i in range(edges):
-    S, E = map(int, stdin.readline().split(' '))
-    adj[S].append(E)
-dijkstra(adj, start)
-if distance.count(target_distance) == 0:
-    print(-1)
-else:
-    for city in range(len(distance)):
-        if distance[city] == target_distance:
-            print(city)
+find_path(maze)
+print(maze[columns-1][rows-1])
