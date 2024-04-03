8358671af88a1673c9759e265bac69945fef960e
구자건 <wkrjsdl5632@gmail.com>
2024-04-03 13:47:59 +0900

    구슬 찾기 정답

--- prac.py
+++ prac.py
@@ -1,74 +1,61 @@
 from sys import stdin
 from collections import deque
+def find_lighter_than_middle(lighter_list, middle_condition):
+    expired_balls = 0
+    for ball in lighter_list:
+        lighter_balls = 0
+        stack = deque()
+        checked_ball = [False for i in range(balls+1)]
+        for i in ball:
+            if not checked_ball[i]:
+                stack.append(i)
+                lighter_balls += 1
+                checked_ball[i] = True
+        while stack:
+            inner_ball = stack.pop()
+            for i in lighter_list[inner_ball]:
+                if not checked_ball[i]:
+                    stack.append(i)
+                    lighter_balls += 1
+                    checked_ball[i] = True
+        if lighter_balls >= middle_condition:
+            expired_balls += 1
+    return expired_balls
 
-def tears_of_the_antarctic():
-    delete_list = list()
-    for glacier in glacier_list:
-        X,Y = coordinate[glacier]
-        for x in range(X-1, X+2):
-            if glacier_map[Y][x] == 0:
-                vertex[glacier] = max(vertex[glacier]-1, 0)
-        for y in range(Y-1, Y+2):
-            if glacier_map[y][X] == 0:
-                vertex[glacier] = max(vertex[glacier]-1, 0)
-        if vertex[glacier] < 1:
-            delete_list.append(glacier)
-    for glacier in glacier_list:
-        x,y = coordinate[glacier]
-        glacier_map[y][x] = vertex[glacier]
-    for delete in delete_list:
-        glacier_list.remove(delete)
-    linked = list()
-    for i in range(len(vertex)):
-        if i in glacier_list:
-            linked.append(False)
-        else:linked.append(True)
-    if glacier_list:
-        start = glacier_list[0]
-        que = deque([start])
-        linked[glacier_list[0]] = True
-        while que:
-            current_node = que.popleft()
-            for next_node in adj[current_node]:
-                if not linked[next_node]:
-                    que.append(next_node)
-                    linked[next_node] = True
-    if False in linked:
-        return False
-    else: return True
+def find_heavier_than_middle(heavier_list, middle_condition):
+    expired_balls = 0
+    for ball in heavier_list:
+        heavier_balls = 0
+        stack = deque()
+        checked_ball = [False for i in range(balls+1)]
+        for i in ball:
+            if not checked_ball[i]:
+                stack.append(i)
+                heavier_balls += 1
+                checked_ball[i] = True
+        while stack:
+            inner_ball = stack.pop()
+            for i in heavier_list[inner_ball]:
+                if not checked_ball[i]:
+                    stack.append(i)
+                    heavier_balls += 1
+                    checked_ball[i] = True
+        if heavier_balls >= middle_condition:
+            expired_balls += 1
+    return expired_balls
 
-column, row = map(int, stdin.readline().split(' '))
-coordinate = list()
-vertex = list()
 
-adj = list()
-glacier_map = list()
-for y in range(column):
-    line = list(map(int, stdin.readline().split(' ')))
-    glacier_map.append(line)
-    
-    for x in range(len(line)):
-        if line[x] != 0:
-            adj.append(list())
-            coordinate.append([x,y])
-            vertex.append(line[x])#
-            for point in range(len(coordinate)):
-                check = coordinate[point]
-                if check[1] == y and check[0] == x-1:
-                    adj[point].append(len(vertex)-1)
-                    adj[len(vertex)-1].append(point)
-                elif check[1] == y-1 and check[0] == x:
-                    adj[point].append(len(vertex)-1)
-                    adj[len(vertex)-1].append(point)
-                
-glacier_list = [i for i in range(len(vertex))]
-count = 0
-condition = True
-while condition:
-    condition = tears_of_the_antarctic()
-    count += 1
-    if sum(vertex) == 0:
-        count = 0
-        condition = False
+balls, edges = map(int, stdin.readline().split(' '))
+heavier_list = [list() for i in range(balls + 1)]
+lighter_list = [list() for i in range(balls + 1)]
+middle_condition = (balls+1) // 2 
+for i in range(edges):
+    heavier, lighter = map(int, stdin.readline().split(' '))
+    heavier_list[lighter].append(heavier)
+    lighter_list[heavier].append(lighter)
 
-print(count)
+result = 0
+result += find_lighter_than_middle(lighter_list, middle_condition)
+result += find_heavier_than_middle(heavier_list, middle_condition)
+
+print(result)
