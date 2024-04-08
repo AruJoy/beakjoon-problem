54a9c5a30fe807b6ddd8fbb83ff5868a0551254d
구자건 <147472859+AruJoy@users.noreply.github.com>
2024-04-08 17:02:55 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 100 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -0,0 +1,13 @@
+from sys import stdin
+
+def LIS(sequence, length_of_sequence):
+    dp = [1] * length_of_sequence
+    for i in range(1, length_of_sequence):
+        for j in range(i):
+            if sequence[i] > sequence[j]:
+                dp[i] = max(dp[i], dp[j] + 1)
+    return max(dp)
+
+length_of_sequence = int(stdin.readline())
+sequence = list(map(int, stdin.readline().split(' ')))
+print(LIS(sequence, length_of_sequence))
