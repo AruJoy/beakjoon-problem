c2941a1ae4cd8970d29adafcb4f4e83a36f8de38
구자건 <147472859+AruJoy@users.noreply.github.com>
2024-04-08 10:41:45 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 1112 ms, Memory: 112408 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
@@ -6,13 +6,13 @@ def matrix_chain(p, number_of_matrix):
         matrix_chain_table[i] = [0 for i in range(number_of_matrix + 1)]
     for j in range(1, number_of_matrix):
         for i in range(1, number_of_matrix+1):
-            list_of_calculation = list()
+            min_value = 10**9
             if number_of_matrix < i+j:
                 continue
             for k in range(1, j+1):
                 value = matrix_chain_table[i][i+j-k] + matrix_chain_table[i+j-k+1][i+j] + p[i-1]*p[i+j-k]*p[i + j]
-                heappush(list_of_calculation, value)
-            matrix_chain_table[i][i+j] = heappop(list_of_calculation)
+                min_value = min(min_value, value)
+            matrix_chain_table[i][i+j] = min_value
     
     return matrix_chain_table[1][number_of_matrix]
 
