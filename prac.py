8ed19aa561559b2087725582356880b73ca588b1
구자건 <wkrjsdl5632@gmail.com>
2024-04-09 19:15:17 +0900

    신입사원 배열

--- prac.py
+++ prac.py
@@ -1,19 +1,24 @@
 from sys import stdin
-def LIS(sequence, length_of_sequence):
-    lis_list = list()
-    max_val = sequence[0]
-    lis_list.append(sequence[0])
-    for i in range(1, length_of_sequence):
-        if max_val < sequence[i]:
-            lis_list.append(sequence[i])
-        else:
-            for j in range(len(lis_list)):
-                if sequence[i] <= lis_list[j]:
-                    lis_list[j] = sequence[i]
-                    break
-        max_val = max(lis_list)
-    return len(lis_list)
 
-length_of_sequence = int(stdin.readline())
-sequence = list(map(int, stdin.readline().split(' ')))
-print(LIS(sequence, length_of_sequence))
+def recruit(n_applicant):
+    applicant_list = [[False for i in range(n_applicant)]for j in range(n_applicant)]
+    for applicant in range(n_applicant):
+        i, j = map(int, stdin.readline().split(' '))
+        applicant_list[i-1][j-1] = True
+    
+    recruit_count = 0
+    end_condition = n_applicant - 1
+    for i in range(n_applicant):
+        for j in range(n_applicant):
+            if applicant_list[i][j] == True:
+                end_condition = j
+                recruit_count += 1
+                break
+            elif j == end_condition:
+                break
+    return recruit_count
+
+test_trials = int(stdin.readline())
+for test in range(test_trials):
+    n_applicant = int(stdin.readline())
+    print(recruit(n_applicant))
